0ea02ffa75bee3dc29c6d30a3e2b905b719dc53f
Cee-Ry <eduaveceasar@gmail.com>
2026-05-29 11:07:57 +0800

    can add multiple number

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+using str = std::string;
 
 int sum(std::vector<int> &arg);
 
@@ -8,16 +9,27 @@ int main(int argc, char *argv[]) {
     std::vector<int> nums;
     char cal_operator;
     int index;
-    
-    for (int i {1}; i < argc; i++) {
-        if ((argc % 2) == 0) {
-            nums.push_back(std::stoi(argv[i]));
+
+    for (int i = 1; i < argc; i++) {
+        if (str(argv[i]) == "+" || str(argv[i]) == "add") { 
+            cal_operator = '+';
+        } else if (str(argv[i]) == "-" || str(argv[i]) == "subtract") {
+            cal_operator = '-';
+        } else if (str(argv[i]) == "*" || str(argv[i]) == "multiply") {
+            cal_operator = '*';
+        } else if (str(argv[i]) == "/" || str(argv[i]) == "divide") {
+            cal_operator = '*';
         } else {
-            std::cout << argv[i];
-        } 
+            nums.push_back(std::stoi(argv[i]));
+        }
     }
 
+    switch (cal_operator) {
+        case '+':
+            std::cout << "Result: " << sum(nums);
+            break;
 
+    }
 
     return 0;
 }
