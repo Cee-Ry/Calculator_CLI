a5b174ea2989f5ea7801e87143baa2ff3ce0a5a2
Cee-Ry <eduaveceasar@gmail.com>
2026-05-30 00:22:43 +0800

    finishing bugs

--- main.cpp
+++ main.cpp
@@ -3,35 +3,57 @@
 #include <string>
 using str = std::string;
 
-int sum(std::vector<int> &arg);
-int difference(std::vector<int> &arg);
-int product(std::vector<int> &arg);
-int quotient(std::vector<int> &arg);
+double sum(std::vector<double> &arg);
+double difference(std::vector<double> &arg);
+double product(std::vector<double> &arg);
+double quotient(std::vector<double> &arg);
 void help();
 
 int main(int argc, char *argv[]) {
-    system("mkdir -p ~/.local/bin");
-
-    std::vector<int> nums;
+    std::vector<double> nums;
     char cal_operator;
-    int index;
     std::string invalid_arg;
+    bool operator_set {};
 
     for (int i = 1; i < argc; i++) {
         if (str(argv[i]) == "+" || str(argv[i]) == "add") { 
+            if (operator_set) {
+                std::cout << "calc: " << argv[i] << ": Invalid Argument\n";
+                std::cout << "Invalid Argument: try '--help' to learn more\n";
+                return 1;
+            }
             cal_operator = '+';
+            operator_set = true;
         } else if (str(argv[i]) == "-" || str(argv[i]) == "subtract") {
+            if (operator_set) {
+                std::cout << "calc: " << argv[i] << ": Invalid Argument\n";
+                std::cout << "Invalid Argument: try '--help' to learn more\n";
+                return 1;
+            }
             cal_operator = '-';
+            operator_set = true;
         } else if (str(argv[i]) == "x" || str(argv[i]) == "multiply") {
+            if (operator_set) {
+                std::cout << "calc: " << argv[i] << ": Invalid Argument\n";
+                std::cout << "Invalid Argument: try '--help' to learn more\n";
+                return 1;
+            }
             cal_operator = 'x';
+            operator_set = true;
         } else if (str(argv[i]) == "/" || str(argv[i]) == "divide") {
+            if (operator_set) {
+                std::cout << "calc: " << argv[i] << ": Invalid Argument\n";
+                std::cout << "Invalid Argument: try '--help' to learn more\n";
+                return 1;
+            }
             cal_operator = '/';
+            operator_set = true;
         } else if (str(argv[i]) == "--help" || str(argv[i]) == "-h") {
             help();
             return 0;
         } else {
             try {
-                nums.push_back(std::stoi(argv[i]));
+                nums.push_back(std::stod(argv[i]));
             } catch (...) {
                 invalid_arg = argv[i];
                 std::cout << "calc: " << invalid_arg << ": Invalid Argument\n";
@@ -43,19 +65,19 @@ int main(int argc, char *argv[]) {
 
     switch (cal_operator) {
         case '+':
-            std::cout << "Result: " << sum(nums);
+            std::cout << "Result: " << sum(nums) << '\n';
             break;
 
         case '-':
-            std::cout << "Result: " << difference(nums);
+            std::cout << "Result: " << difference(nums) << '\n';
             break;
 
         case 'x':
-            std::cout << "Result: " << product(nums);
+            std::cout << "Result: " << product(nums) << '\n';
             break;
 
         case '/':
-            std::cout << "Result: " << quotient(nums);
+            std::cout << "Result: " << quotient(nums) << '\n';
             break;
             
         default:
@@ -66,8 +88,8 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int sum(std::vector<int> &arg) {
-    int adding {};
+double sum(std::vector<double> &arg) {
+    double adding {};
     for (int i {}; i < arg.size(); i++) {
         adding += arg[i];
     }
@@ -75,8 +97,8 @@ int sum(std::vector<int> &arg) {
     return adding;
 }
 
-int difference(std::vector<int> &arg) {
-    int result {};
+double difference(std::vector<double> &arg) {
+    double result {};
     result = arg[0];
     for (int i {1}; i < arg.size(); i++) {
         result -= arg[i];
@@ -85,8 +107,8 @@ int difference(std::vector<int> &arg) {
     return result;
 }
 
-int product(std::vector<int> &arg) {
-    int result {};
+double product(std::vector<double> &arg) {
+    double result {};
     result = arg[0];
     for (int i {1}; i < arg.size(); i++) {
         result *= arg[i];
@@ -95,11 +117,16 @@ int product(std::vector<int> &arg) {
     return result;
 }
 
-int quotient(std::vector<int> &arg) {
-    int result {};
+double quotient(std::vector<double> &arg) {
+    double result {};
     result = arg[0];
     for (int i {1}; i < arg.size(); i++) {
         result /= arg[i];
+        if (arg[i] == 0) {
+            std::cout << "Invalid Argument: division by zero\n";
+            std::cout << "Invalid Argument: try '--help' to learn more\n";
+            exit(1);
+        }
     }
 
     return result;
@@ -117,6 +144,9 @@ void help() {
     std::cout << "   -h, --help        Display this help and exit the program\n";
 
     std::cout << "\nExamples:\n";
-    std::cout << "   calc add 3 2 5 10\n";
+    std::cout << "   calc add 3 2 5 10 ||\n";
+    std::cout << "   calc - 4 5 ||\n";
+    std::cout << "   calc 4 multiply 5 ||\n";
+    std::cout << "   calc 4 / 5 \n";
     std::cout << "   result: 20\n";
 }
